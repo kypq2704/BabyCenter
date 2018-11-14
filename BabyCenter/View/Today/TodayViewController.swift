6aca42dfabdbab24bf68120193be472f22c76f66
Phan Quy Ky <ky.pq@altplus.com.vn>
2018-11-14 16:00:49 +0700

    update

---
+++ BabyCenter/View/Today/TodayViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TodayViewController.swift
+//  BabyCenter
+//
+//  Created by Phan Quy KY on 2018/11/14.
+//  Copyright © 2018 Phan Quy KY. All rights reserved.
+//
+
+import UIKit
+
+class TodayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func showChildSettingView(_ sender: Any) {
+        
+    }
+    
+    @IBAction func showImageView(_ sender: Any) {
+        
+    }
+    @IBAction func showTimeLineView(_ sender: Any) {
+    
+    }
+}
